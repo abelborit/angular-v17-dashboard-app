27e7d520970be3435d39d6b86cddeab2b8530e8e
Abel Borit <abelboritguitton@gmail.com>
2024-02-19 14:51:12 -0500

    [FEAT] users-page.component using "ng g component dashboard/pages/usersPage --skip-selector"

---
+++ src/app/dashboard/pages/users-page/users-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './users-page.component.html',
+  styleUrl: './users-page.component.css'
+})
+export class UsersPageComponent {
+
+}
