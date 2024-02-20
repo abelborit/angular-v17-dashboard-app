dc0b721d7b51a60ae25c8718e3255d9b321ead05
Abel Borit <abelboritguitton@gmail.com>
2024-02-20 11:39:45 -0500

    [FEAT] add @defer blocks

--- src/app/dashboard/pages/defer-views-page/defer-views-page.component.ts
+++ src/app/dashboard/pages/defer-views-page/defer-views-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
 
 @Component({
   selector: 'app-defer-views-page',
   standalone: true,
-  imports: [],
+  imports: [HeavyLoadersSlowComponent],
   templateUrl: './defer-views-page.component.html',
   styleUrl: './defer-views-page.component.css',
 })
 export default class DeferViewsPageComponent {}
+
+/* ************************************************************************************************************************ */
+/* los defer views o defer blocks o defer functions, tiene varios nombres pero son lo mismo, es un comportamiento que podemos añadir a los templates HTML para decirles cómo queremos que cargue el componente lo cual lleva al lazy load al siguiente nivel. Al usar los defer views es una forma de cargar el componente de manera diferida, es decir, de forma perezosa pero diferida, como si fuera una carga bajo demanda, es decir, hasta que se necesite recién carga */
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
-  template: `<h1>HeavyLoadersSlowComponent</h1>`,
+  imports: [CommonModule],
+  template: ` <section [ngClass]="['w-full h-[600px]', cssClass]">
+    <h1>Heavy Loaders Slow Component</h1>
+  </section>`,
 })
-export class HeavyLoadersSlowComponent {}
+export class HeavyLoadersSlowComponent {
+  @Input({ required: true }) cssClass!: string;
+
+  /* este será un componente pesado y bloqueante cosa que NO se debería hacer de esta forma, solo se hará para fines de práctica y ver las funcionalidades de los @defer en defer-views-page.component */
+  constructor() {
+    console.log('Heavy Loaders Slow Component - Cargando... ❌');
+    const start = Date.now();
+
+    /* este es un código que bloquea todo */
+    while (Date.now() - start < 3000) {}
+    console.log('Heavy Loaders Slow Component - Cargado ✅');
+  }
+}
