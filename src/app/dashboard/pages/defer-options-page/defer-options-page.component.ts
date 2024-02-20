3bfd3172d15bfde3638bc09b5216481eeea7381f
Abel Borit <abelboritguitton@gmail.com>
2024-02-20 14:10:53 -0500

    [FEAT] add @defer triggers options

--- src/app/dashboard/pages/defer-options-page/defer-options-page.component.ts
+++ src/app/dashboard/pages/defer-options-page/defer-options-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeavyLoadersFastComponent } from '@shared/heavy-loaders/heavy-loaders-fast.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-options-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeavyLoadersFastComponent, TitleComponent],
   templateUrl: './defer-options-page.component.html',
   styleUrl: './defer-options-page.component.css',
 })
-export default class DeferOptionsPageComponent {}
+export default class DeferOptionsPageComponent {
+  /* se colocó esto solo para simular que el botón se hizo click una vez y luego se deshabilitó ya que se mostró la información */
+  public isDisabledBtn = signal<boolean>(false);
+
+  disabledBtn() {
+    this.isDisabledBtn.update(() => true);
+  }
+}
--- src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-fast.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
-  template: `<h1>HeavyLoadersFastComponent</h1>`,
+  imports: [CommonModule],
+  template: `<section [ngClass]="['w-full h-[600px]', cssClass]">
+    <!-- <h1>Heavy Loaders Fast Component</h1> -->
+
+    <!-- con este <ng-content /> se convierte en un HOC (High Order Component) que desde el padre se pueden mandar los componentes hijos -->
+    <ng-content />
+  </section>`,
 })
-export class HeavyLoadersFastComponent {}
+export class HeavyLoadersFastComponent {
+  @Input({ required: true }) cssClass!: string;
+
+  constructor() {
+    console.log('Heavy Loaders Fast Component - Cargado ✅');
+  }
+}
