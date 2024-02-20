ee3b71c27acc2d49898173c5ab02e3ca3abdaee0
Abel Borit <abelboritguitton@gmail.com>
2024-02-20 10:34:42 -0500

    [FEAT] update template title.component.ts

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -4,7 +4,10 @@ import { Component, Input, booleanAttribute } from '@angular/core';
   selector: 'app-title',
   standalone: true,
   imports: [],
-  template: ` <h1 class="text-3xl mb-5">{{ title }} - {{ withShadow }}</h1> `,
+  template: `
+    <h1 class="text-3xl mb-5">{{ title }}</h1>
+    <h3 class="text-xl">With attribute props: {{ withShadow }}</h3>
+  `,
 })
 export class TitleComponent {
   /* aquí se puede colocar title!: string con el ! o sino title: string = "" que es inicializarlo con el string vacio. En este caso se coloca con ! ya que como se le está diciendo que es un campo requerido entonces sí o sí tiene que venir, pero también se puede inicialiar con un string vacío */
